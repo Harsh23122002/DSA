69a58641237bb10b4e02513a53e3e4c44130bae0
Harsh23122002 <harshgwaghmare@gmail.com>
2022-07-12 23:39:04 +0530

    linked list create function implemented

--- ds/linked_list.cpp
+++ ds/linked_list.cpp
@@ -7,3 +7,28 @@ struct Node{
     Node* next;
 };
 
+Node* first = NULL;
+Node* last = NULL;
+
+void Create(){
+    Node* temp=NULL;
+    int a;
+    if(first==NULL){
+        first = new Node();
+        cout<<"Enter the number to be stored in the first node: \n";
+        cin>>a;
+        first->data=a;
+        first->next=NULL;
+        last = first;
+    }
+    else{
+        temp = new Node();
+        cout<<"Enter the number to be stored in the node: \n";
+        cin>>a;
+        temp->data=a;
+        temp->next=NULL;
+        last->next=temp;
+        last=temp;
+    }
+
+}
